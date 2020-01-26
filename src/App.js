6c54b6b0b44d8a2189685c861187cb265847e748
kangkung17 <mmuhdar17@gmail.com>
2020-01-27 00:01:16 +0700

    Les 1. setting pages dan route nya

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
-function App() {
-  return <h1>Let's start!</h1>;
-}
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+
+const App = () => {
+  return <Router>
+    <Switch> 
+    <Route path="/" exact> 
+      <Users />
+    </Route>
+    <Route path="/places/new" exact>
+      <NewPlace />
+    </Route>
+    <Redirect to="/" /> 
+    </Switch>
+  </Router> 
+};
 
 export default App;
+
+// Redirect => ini untuk ketika router di ketik selain dari router terdaftar maka kembali ke page default
+
+// Switch => ini untuk ketika router tujuan di akses tdk ke Redirect ke page default
