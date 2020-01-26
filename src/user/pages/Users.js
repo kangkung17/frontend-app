3b7629afb8006d4937a3088e43eda36b6553a821
kangkung17 <mmuhdar17@gmail.com>
2020-01-27 00:35:30 +0700

    les 2. menambahkan UserList page/ component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import UserList from '../components/UsersList';
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-    return <h2>Users works!</h2>;
+
+    const USER = [{
+        id: 'u1',
+        name: 'Muhammad Muhdar',
+        image: 'https://images.pexels.com/photos/3551207/pexels-photo-3551207.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        places: 3
+    }]
+
+    return <UsersList item={USER} />;
 };
 
 export default Users;
