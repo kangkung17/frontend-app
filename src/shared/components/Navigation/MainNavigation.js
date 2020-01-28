c02b8ce86f0f4f20659aa1e611b3c6491a41f331
kangkung17 <mmuhdar17@gmail.com>
2020-01-29 05:02:49 +0700

    Les 8. handling the drawer state

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,36 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
 import SideDrawer from './SideDrawer';
-import MainHeader from './MainHeader';
+import Backdrop from '../UIElements/Backdrop';
 import './MainNavigation.css';
 
 const MainNavigation = props => {
-    return (
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawer = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawer = () => {
+    setDrawerIsOpen(false);
+  };
+
+  return (
     <React.Fragment>
-    <SideDrawer>
-        <nav className="main-navigation__drawer-nav">
+      {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
+      {drawerIsOpen && (
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
             <NavLinks />
-        </nav>
-    </SideDrawer>
-    <MainHeader>
-        <button className="main-navigation__menu-btn">
-            <span />
-            <span />
-            <span />
+          </nav>
+        </SideDrawer>
+      )}
+      <MainHeader>
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
+          <span />
+          <span />
+          <span />
         </button>
         <h1 className="main-navigation__title">
-            <Link to="/">YourPlaces</Link>
+          <Link to="/">YourPlaces</Link>
         </h1>
         <nav className="main-navigation__header-nav">
-            <NavLinks />
+          <NavLinks />
         </nav>
-    </MainHeader>
+      </MainHeader>
     </React.Fragment>
-    );
+  );
 };
 
 export default MainNavigation;
-
 // fungsi React.Fragment untuk me return multiple root elements
