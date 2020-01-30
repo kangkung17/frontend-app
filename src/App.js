3b809e78b2a6b2e90f1c6cd22e97c744cbe4de0c
kangkung17 <mmuhdar17@gmail.com>
2020-01-30 17:36:12 +0700

    les 8. rendering user places & using dynamic route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import UserPlaces from './places/pages/UserPlaces';
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 
@@ -12,6 +13,9 @@ const App = () => {
     <Switch> 
     <Route path="/" exact> 
       <Users />
+    </Route>
+    <Route path="/:userId/places" exact>
+      <UserPlaces /> 
     </Route>
     <Route path="/places/new" exact>
       <NewPlace />
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -9,7 +9,7 @@ const Users = () => {
         id: 'u1',
         name: 'Muhammad Muhdar',
         image: 'https://images.pexels.com/photos/3497181/pexels-photo-3497181.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-        places: 1
+        places: 4
       }
     ];  
 
