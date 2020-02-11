814683c357f8ef1101384b66b5c16b63c1d970ad
kangkung17 <mmuhdar17@gmail.com>
2020-02-11 23:10:31 +0700

    adding custom buttons

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
 
-import MainNavigation from './shared/components/Navigation/MainNavigation';
-import UserPlaces from './places/pages/UserPlaces';
-import Users from './user/pages/Users';
-import NewPlace from './places/pages/NewPlace';
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UserPlaces from "./places/pages/UserPlaces";
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
 const App = () => {
-  return <Router>
-    <MainNavigation />
-    <main>
-    <Switch> 
-    <Route path="/" exact> 
-      <Users />
-    </Route>
-    <Route path="/:userId/places" exact>
-      <UserPlaces /> 
-    </Route>
-    <Route path="/places/new" exact>
-      <NewPlace />
-    </Route>
-    <Redirect to="/" /> 
-    </Switch>
-    </main>
-  </Router> 
+  return (
+    <Router>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
+    </Router>
+  );
 };
 
 export default App;
 
-// Redirect => ini untuk ketika router di ketik selain dari router terdaftar maka kembali ke page default
+// ! Redirect => ini untuk ketika router di ketik selain dari router terdaftar maka kembali ke page default
 
-// Switch => ini untuk ketika router tujuan di akses tdk ke Redirect ke page default
+// ! Switch => ini untuk ketika router tujuan di akses tdk ke Redirect ke page default
